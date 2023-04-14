391be371b9ec8ff5515be1dcd6ae33a67b9da9d6
Nathan Kester <natekester@Nates-MacBook-Pro.local>
2023-04-14 14:26:29 -0700

    add linkedin link

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -8,8 +8,14 @@ function Header() {
       <div className={styles.flexboxContainer}>
         <h1 className={styles.nate}>Nate Kester</h1>
         <h2 className={styles.title}>Full Stack Engineer</h2>
-        <a className={styles.email} href="mailto:nate.d.kester@gmail.com">nate.d.kester@gmail.com</a>
-        
+        <div className={styles.linksContainer}>
+          <a className={styles.email} href="mailto:nate.d.kester@gmail.com">
+            nate.d.kester@gmail.com
+          </a>
+          <a href="https://www.linkedin.com/in/nathan-kester-5512b193/">
+            <img src="linkedinIcon.png" className={styles.linkedin} />
+          </a>
+        </div>
         <nav className={styles.tabs}>
           <NavLink
             className={(isActive) =>
