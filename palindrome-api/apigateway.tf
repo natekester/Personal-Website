65de568ad51d7fc4052e8d763eb50b7b7f3dd21b
Nathan Kester <natekester@Nates-MacBook-Pro.local>
2023-04-20 16:10:19 -0700

    change path for apigateway to not be a slip of the tongue

--- palindrome-api/apigateway.tf
+++ palindrome-api/apigateway.tf
@@ -16,7 +16,7 @@ resource "aws_api_gateway_rest_api" "api" {
 }
 
 resource "aws_api_gateway_resource" "resource" {
-  path_part   = "is-lambda"
+  path_part   = "is-palindrome"
   parent_id   = aws_api_gateway_rest_api.api.root_resource_id
   rest_api_id = aws_api_gateway_rest_api.api.id
 }
