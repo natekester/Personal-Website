c91b4f30a96f082b83b6430a9063e5f92ddbda53
Nathan Kester <natekester@Nates-MacBook-Pro.local>
2023-04-20 20:48:52 -0700

    add user event tests for the header

--- UI/src/App.test.js
+++ UI/src/App.test.js
@@ -2,10 +2,39 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 import '@testing-library/jest-dom';
+import userEvent from '@testing-library/user-event';
 
 test('makes sure that the page generally loads', () => {
   render(<App />);
 
   expect(screen.getByText('Nate Kester')).toBeInTheDocument();
   expect(screen.getByText('nate.d.kester@gmail.com')).toBeInTheDocument();
+});
+
+test('Check that Each page renders when clicking the text', async () => {
+  render(<App />);
+
+  //Generally picking "solid" elements of each page to ensure navbar works
+
+  //check About Me page Renders
+  const AboutMeNav = screen.getByText('About Me');
+  await userEvent.click(AboutMeNav);
+  expect(screen.getByText("Hi! I'm Nate")).toBeInTheDocument();
+
+  //check Projects page Renders
+  const projectsNav = screen.getByText('Projects');
+  await userEvent.click(projectsNav);
+  expect(screen.getByText('This Web Page!')).toBeInTheDocument();
+
+  //check Career page Renders
+  const careerNav = screen.getByText('Career');
+  await userEvent.click(careerNav);
+  expect(
+    screen.getByText('Software Engineer II (Remote) - Pluralsight')
+  ).toBeInTheDocument();
+
+  //check Educationpage Renders
+  const educationNav = screen.getByText('Education');
+  await userEvent.click(educationNav);
+  expect(screen.getByText('University of Utah')).toBeInTheDocument();
 });
--- UI/src/components/Header/Header.test.js
+++ UI/src/components/Header/Header.test.js
@@ -1,6 +1,15 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import App from './App';
 import '@testing-library/jest-dom';
+import { BrowserRouter, Router } from 'react-router-dom';
+import Header from './Header';
 
-test('TODO', () => {});
+test('makes sure that the page generally loads', async () => {
+  render(<Header />, { wrapper: BrowserRouter });
+
+  expect(screen.getByText('Nate Kester')).toBeInTheDocument();
+  expect(screen.getByText('About Me')).toBeInTheDocument();
+  expect(screen.getByText('Career')).toBeInTheDocument();
+  expect(screen.getByText('Projects')).toBeInTheDocument();
+  expect(screen.getByText('Education')).toBeInTheDocument();
+});
