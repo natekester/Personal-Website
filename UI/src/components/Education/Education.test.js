2765976ff112ac4ba41b361ae11f6c12a7bff1ae
Nathan Kester <natekester@Nates-MacBook-Pro.local>
2023-04-20 20:53:15 -0700

    add test for Education component and key render elements

--- UI/src/components/Education/Education.test.js
+++ UI/src/components/Education/Education.test.js
@@ -1,6 +1,19 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import App from './App';
 import '@testing-library/jest-dom';
+import Education from './Education';
 
-test('TODO', () => {});
+test('makes sure that the page generally loads', async () => {
+  render(<Education />);
+
+  expect(screen.getByText('University of Utah')).toBeInTheDocument();
+  expect(screen.getByText('2011 - 2015')).toBeInTheDocument();
+  expect(screen.getByText('B.S. Materials Engineering')).toBeInTheDocument();
+  expect(screen.getByText('UCSD - Multiple CS Courses')).toBeInTheDocument();
+  expect(
+    screen.getByText('AWS - Practitioner Certification')
+  ).toBeInTheDocument();
+  expect(
+    screen.getByText('Harvard - CS50 Web Development')
+  ).toBeInTheDocument();
+});
