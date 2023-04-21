3dd42c78b0624d52a1624f253d93402608bdded4
Nathan Kester <natekester@Nates-MacBook-Pro.local>
2023-04-21 09:46:17 -0700

    adjust lambda handler. Add pytest infra and handler test.

--- palindrome-api/palindrome/app.py
+++ palindrome-api/palindrome/app.py
@@ -3,15 +3,32 @@ import boto3
 
 
 def palindrome_lambda_handler(event, context):
-    is_palindrome = "there was no word"
-    if event.get('queryStringParameters'):
-        if event.get('queryStringParameters').get('word'):
-            word = event['queryStringParameters']['word']
-            is_palindrome = word == word[::-1]
+    # if the request is malformed - send 400
+    try:
+        word = event["queryStringParameters"]["word"]
+        if not isinstance(word, str):
+            raise
+        if len(word) > 19:
+            raise
+
+    except:
+        return {
+            "statusCode": 400,
+            "body": json.dumps(
+                {
+                    "message": "you must send the 'word' query parameter with less than 20 characters",
+                }
+            ),
+        }
+
+    flipped_word = word[::-1]
+    is_palindrome = word == flipped_word
 
     return {
         "statusCode": 200,
-        "body": json.dumps({
-            "isPalindrome": is_palindrome,
-        })
+        "body": json.dumps(
+            {
+                "isPalindrome": is_palindrome,
+            }
+        ),
     }
