c3f4b1bdb773e1316fed1071bd4289e5aada9ca6
Nathan Kester <natekester@Nates-MacBook-Pro.local>
2023-05-16 15:26:35 -0700

    fix palindrome testing by making lowercase

--- UI/src/api-calls/palindrome.js
+++ UI/src/api-calls/palindrome.js
@@ -1,11 +1,12 @@
 import { localPalindromeApi, prodPalindromeApi } from '../constants/api-urls';
 
 export async function callPalindrome(word) {
+  const lowerCaseWord = word.toLowerCase();
   const api =
     process.env.NODE_ENV === 'development'
       ? localPalindromeApi
       : prodPalindromeApi;
-  const url = `${api}?word=${word}`;
+  const url = `${api}?word=${lowerCaseWord}`;
 
   const response = await fetch(url, { mode: 'cors' });
   const jsonData = await response.json();
