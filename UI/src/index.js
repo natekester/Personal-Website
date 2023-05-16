bdb1d437dd3238489f4e6c2fe4631967e4dd6daa
Nathan Kester <natekester@Nates-MacBook-Pro.local>
2023-05-15 17:47:04 -0700

    stop console logs in production

--- UI/src/index.js
+++ UI/src/index.js
@@ -3,6 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
+if (process.env.NODE_ENV === 'production') {
+  console.log = () => {};
+  console.error = () => {};
+  console.debug = () => {};
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
